076ca14afae5a54907fad817283edc23e7990bec
Feriandra <112842241+feriandra@users.noreply.github.com>
2025-10-21 08:11:50 +0700

    Update footer.php typo

--- application/views/template_page_user/footer.php
+++ application/views/template_page_user/footer.php
@@ -12,9 +12,6 @@
                 </script> © webadmin.
             </div>
             <div class="col-sm-6">
-                <div class="text-sm-end d-none d-sm-block">
-                    Crafted with <i class="mdi mdi-heart text-danger"></i> by <a href="https://Themesdesign.com/" target="_blank" class="text-reset">Themesdesign</a>
-                </div>
             </div>
         </div>
     </div>
